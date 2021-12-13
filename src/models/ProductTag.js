9ce9b5b5524d88a0b2101b664dffb03d6c6451b0
Darion Richards <darionrichards@outlook.com>
2021-12-13 13:57:01 +0000

    Fixed typo

--- src/models/ProductTag.js
+++ src/models/ProductTag.js
@@ -14,7 +14,7 @@ const schema = {
     productId: {
         type: DataTypes.INTEGER,
         reference: {
-            model: "Product",
+            model: "product",
             key: "id",
         },
     },
