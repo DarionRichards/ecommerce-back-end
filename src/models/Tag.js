fbacab578253c8ae3b9b858a0ac393ee7cf3bec3
Darion Richards <darionrichards@outlook.com>
2021-12-13 13:27:18 +0000

    Updated Tag model
    - created schema
    - created options
    - declared records/columns

--- src/models/Tag.js
+++ src/models/Tag.js
@@ -1,20 +1,29 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Tag extends Model {}
 
-Tag.init(
-  {
-    // define columns
-  },
-  {
+const schema = {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+    },
+    tagName: {
+        type: DataTypes.STRING,
+    },
+};
+
+const options = {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'tag',
-  }
-);
+    modelName: "tag",
+};
+
+Tag.init(schema, options);
 
 module.exports = Tag;
